ae819a425719dcdb61c045867308ce55d5902d3c
mvuko01 <mvuko01@fesb.hr>
2020-11-04 16:46:30 +0100

    comm

--- zadatak_2.c
+++ zadatak_2.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -93,11 +94,144 @@ int Obrisi(char prez[], pozicija p)
 	return 0;
 }
 
+int DodajIzad(char prez[], int x, char novoime[], char novoprezime[], pozicija p)
+{
+	pozicija q;
+
+	p = Pronadi(prez, p->next);
+	if (p == NULL) {
+		printf("\nNema trazenog elementa!");
+		return 0;
+	}
+	else {
+		q = (pozicija)malloc(sizeof(struct osoba));
+		q->god_rodenja = x;
+		strcpy(q->ime, novoime);
+		strcpy(q->prezime, novoprezime);
+
+		q->next = p->next;
+		p->next = q;
+		return 0;
+	}
+
+
+}
+
+int DodajIspred(char prez[], int x, char novoime[], char novoprezime[], pozicija p)
+{
+	pozicija q, f;
+
+	q = Pronadi(prez, p->next);
+	if (q == NULL) {
+		printf("\nNema trazenog elementa!");
+		return 0;
+	}
+	else
+	{
+		while (p->next != q)
+			p = p->next;
+
+		f = (pozicija)malloc(sizeof(struct osoba));
+		f->god_rodenja = x;
+		strcpy(f->ime, novoime);
+		strcpy(f->prezime, novoprezime);
+
+		f->next = p->next;
+		p->next = f;
+		return 0;
+	}
+}
+
+int ObrisiSve(pozicija p)
+{
+	
+	pozicija temp;
+
+	while (p->next != NULL)
+	{
+		temp = p->next;
+		p->next = temp->next;
+		free(temp);
+		
+	}
+	return 0;
+}
+
+int UpisDatoteka(pozicija p, FILE* f)
+{
+	fputs("IME\tPREZIME\t    GODINA RODENJA\n", f);
+	while (p->next != NULL)
+	{
+		p = p->next;
+		fprintf(f, "%s\t%s\t\t%d.\n", p->ime, p->prezime, p->god_rodenja);
+
+	}
+	return 0;
+
+}
+
+int UcitajDatoteka(pozicija p, FILE* f)
+{
+	int br = 0, i;
+	char str[50];
+	char first[50], last[50];
+	int year;
+
+	while (!feof(f))
+	{
+		fgets(str, 50, f);
+		br++;
+
+	}
+	rewind(f);
+
+	for (i = 0; i < br; i++)
+	{
+		fscanf(f, "%s %s %d", first, last, &year);
+		UnosK(year, first, last, p);
+	}
+
+	return 0;
+}
+
+int Sortiraj(pozicija p)
+{
+	pozicija end = NULL, j, prev_j, temp;
+
+	while (p->next != end)
+	{
+		prev_j = p;
+		j = p->next;
+		while (j->next != end)
+		{
+			if (strcmp(j->prezime, j->next->prezime) > 0)
+			{
+				temp = j->next;
+				prev_j->next = temp;
+				j->next = temp->next;
+				temp->next = j;
+
+				j = temp;
+			}
+			prev_j = j;
+			j = j->next;
+
+		}
+		end = j;
+
+	}
+	return 0;
+
+
+}
+
 int main()
 {
 	struct osoba Head;
 	Head.next = NULL;
-	pozicija q;
+	FILE* fo = NULL;
+	FILE* fp = NULL;
+	//pozicija q;
 
 	UnosP(2000, "Ana", "Anic", &Head);
 	UnosK(2002, "Ivo", "Ivic", &Head);
@@ -115,5 +249,28 @@ int main()
 	UnosK(2100, "TTTT", "LLL", &Head);
 	UnosP(2000, "lili", "vivi", &Head);
 	Ispisi(Head.next);
+	Sortiraj(&Head);
+	puts("\n\n");
+	Ispisi(Head.next);
+
+	fo = fopen("Popis.txt", "w");
+	fp = fopen("citat.txt", "r");
+	if (fo == NULL || fp == NULL)
+	{
+		printf("Datoteka nije otvorena!");
+		return -1;
+	}
+	
+	UpisDatoteka(&Head, fo);
+	//puts("----------------");
+	//ObrisiSve(&Head);
+	//Ispisi(Head.next);
+	
+	fclose(fo);
+	ObrisiSve(&Head);
+	//UcitajDatoteka(&Head, fp);
+	//Ispisi(Head.next);
+	fclose(fp);
+
 	return 0;
 }
